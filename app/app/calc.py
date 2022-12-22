bf9288e1928d389181d2dcf24c21b981d6bbf79f
deepak <deepak.p@qburst.com>
2022-12-22 12:53:25 +0530

    for finding error

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+calculator functions
+"""
+
+
+def add(x,y,z):
+    """add x, y, z and returns sum"""
+    return x+y+z
+
+def sub(x,y):
+    return x-y
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+sample test
+"""
+
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """tests the calc module"""
+    def test_add_num(self):
+        """test for adding nums"""
+        res = calc.add(1,9,1)
+        self.assertEqual(res,11)
+    
+    def test_sub_nums(self):
+        """test for sub nums"""
+        res = calc.sub(10,4)
+        self.assertEqual(res, 6)
+
+
